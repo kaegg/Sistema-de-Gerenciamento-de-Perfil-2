e46789500e458755d881dba3d0bf829eef287392
Leonardo Almenara <leonardoferreira.uk@gmail.com>
2024-08-31 20:17:59 -0300

    Controler do comentarios

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comentario; // Assumindo que existe um modelo Comentario
+use Validator;
 
 class ComentarioController extends Controller
 {
-    //
+    // Função para listar todos os comentários
+    public function index()
+    {
+        // Busca todos os comentários do banco de dados
+        $comentarios = Comentario::all();
+
+        // Passa os comentários para a view
+        return view('comentarios.index', compact('comentarios'));
+    }
+
+    // Função para adicionar um novo comentário
+    public function store(Request $request)
+    {
+        // Validação dos dados do formulário
+        $validator = Validator::make($request->all(), [
+            'comentario' => 'required',
+        ], [
+            'comentario.required' => 'Por favor, insira um comentário.',
+        ]);
+
+        if ($validator->fails()) {
+            // Retorna para a página anterior com os erros e os dados do formulário
+            return back()->withErrors($validator)->withInput();
+        }
+
+        // Criação do comentário
+        Comentario::create([
+            'comentario' => $request->comentario,
+        ]);
+
+        // Redireciona de volta para a página de comentários com uma mensagem de sucesso
+        return redirect()->route('comentarios.index')->with('success', 'Comentário adicionado com sucesso!');
+    }
 }
